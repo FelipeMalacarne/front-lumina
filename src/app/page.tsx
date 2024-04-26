17d9052121afc33c8f287fc5b3356d32d74f26fd
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-04-26 14:17:01 -0300

    next setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const metadata = {
+    title: 'Lumina',
+}
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        </main>
+    );
+}
