208a9c2395fd4899d53f07323800edd97b816fbe
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-04 21:17:38 -0300

    #6 navbar done

--- src/components/navigation/nav-bar.tsx
+++ src/components/navigation/nav-bar.tsx
@@ -14,11 +14,13 @@ const Navbar = ({ user, logout }: NavbarProps) => {
 
     return (
         <>
-            <div className="hidden flex-col md:flex">
+            <div className="flex-col flex">
                 <div className="border-b">
                     <div className="flex h-16 items-center px-4">
-                        <ProjectSwitcher />
-                        <MainNav className="mx-6" />
+                        <div className="flex">
+                            <MainNav className="mx-6 md:order-2" />
+                            <ProjectSwitcher className="mx-1 md:order-1 " />
+                        </div>
 
                         <div className="ml-auto flex items-center space-x-4">
                             <ModeToggle />
