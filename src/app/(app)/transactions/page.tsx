9279da7abbee6846188312c84497a6976bc383b6
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-29 16:08:56 -0300

    #8 transactions filter and export button

--- src/app/(app)/transactions/page.tsx
+++ src/app/(app)/transactions/page.tsx
@@ -1,6 +1,10 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { AddTransactionCard } from "./components/add-transaction-card";
 import { MonthlyCard } from "./components/monthly-card";
 import { WeeklyCard } from "./components/weekly-card";
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { File, ListFilter } from "lucide-react";
 
 export const metadata = {
     title: "Transactions",
@@ -11,11 +15,11 @@ const TransactionsPage = () => {
 
     return (
         <div className="flex-1 space-y-8 p-8 pt-6">
-            <div className="flex items-center justify-between">
-
-                <h2 className="text-3xl font-bold tracking-tight">Transações</h2>
-
-            </div>
+            {/* <div className="flex items-center justify-between"> */}
+            {/**/}
+            {/*     <h2 className="text-3xl font-bold tracking-tight">Transações</h2> */}
+            {/**/}
+            {/* </div> */}
 
             <div className="space-y-4">
                 <main className="grid flex-1 items-start gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
@@ -25,6 +29,50 @@ const TransactionsPage = () => {
                             <WeeklyCard />
                             <MonthlyCard />
                         </div>
+                        <Tabs defaultValue="all">
+                            <div className="flex items-center">
+                                <TabsList>
+                                    <TabsTrigger value="all">Todas</TabsTrigger>
+                                    <TabsTrigger value="incomes">Ganhos</TabsTrigger>
+                                    <TabsTrigger value="expenses">Despesas</TabsTrigger>
+                                </TabsList>
+                                <div className="ml-auto flex items-center gap-2">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                variant="outline"
+                                                size="sm"
+                                                className="h-7 gap-1 text-sm"
+                                            >
+                                                <ListFilter className="h-3.5 w-3.5" />
+                                                <span className="sr-only sm:not-sr-only">Filter</span>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuCheckboxItem checked>
+                                                Fulfilled
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem>
+                                                Declined
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem>
+                                                Refunded
+                                            </DropdownMenuCheckboxItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                    <Button
+                                        size="sm"
+                                        variant="outline"
+                                        className="h-7 gap-1 text-sm"
+                                    >
+                                        <File className="h-3.5 w-3.5" />
+                                        <span className="sr-only sm:not-sr-only">Export</span>
+                                    </Button>
+                                </div>
+                            </div>
+                        </Tabs>
 
                     </div>
 
