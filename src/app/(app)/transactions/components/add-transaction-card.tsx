5b0c52fc100ca882577f9e7bcdd9d42058160329
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-05-29 09:32:28 -0300

    #8 Transactions top cards

---
+++ src/app/(app)/transactions/components/add-transaction-card.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+
+export const AddTransactionCard = () => {
+    return (
+        <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0" >
+            <CardHeader className="pb-3">
+                <CardTitle>Suas Transações</CardTitle>
+                <CardDescription className="max-w-lg text-balance leading-relaxed">
+                    Adicione suas transações para ter um controle financeiro mais preciso.
+                </CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <Button>Adicione transações</Button>
+            </CardFooter>
+        </Card>
+    )
+}
--- src/app/(app)/transactions/page.tsx
+++ src/app/(app)/transactions/page.tsx
@@ -1,3 +1,6 @@
+import { AddTransactionCard } from "./components/add-transaction-card";
+import { MonthlyCard } from "./components/monthly-card";
+import { WeeklyCard } from "./components/weekly-card";
 
 export const metadata = {
     title: "Transactions",
@@ -7,9 +10,29 @@ export const metadata = {
 const TransactionsPage = () => {
 
     return (
-        <>
+        <div className="flex-1 space-y-8 p-8 pt-6">
+            <div className="flex items-center justify-between">
 
-        </>
+                <h2 className="text-3xl font-bold tracking-tight">Transações</h2>
+
+            </div>
+
+            <div className="space-y-4">
+                <main className="grid flex-1 items-start gap-4 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+                    <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+                        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+                            <AddTransactionCard />
+                            <WeeklyCard />
+                            <MonthlyCard />
+                        </div>
+
+                    </div>
+
+                </main>
+
+            </div>
+
+        </div>
     )
 }
 
