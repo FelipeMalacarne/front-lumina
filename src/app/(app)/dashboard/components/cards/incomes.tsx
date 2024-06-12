ea33931fee25d5bd526f28fa565c8d9041c21032
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-06-12 10:06:04 -0300

    #12 Incomes card with api request

--- src/app/(app)/dashboard/components/cards/incomes.tsx
+++ src/app/(app)/dashboard/components/cards/incomes.tsx
@@ -1,7 +1,30 @@
+'use client'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import axios from "@/lib/axios";
 import { ArrowUp } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function IncomesCard() {
+    const [incomes, setIncomes] = useState<string>("R$ 0,00")
+    const [percentageChange, setPercentageChange] = useState<string>("+0%")
+
+    const getIncomes = async () => {
+        try {
+            const response = await axios<{ monthly_income: number, percentage_change: string }>('/api/dashboard/monthly-income')
+
+            setIncomes(new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(response.data.monthly_income / 100))
+            setPercentageChange(response.data.percentage_change)
+
+        } catch (error: any) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getIncomes()
+    }, [])
+
+
     return (
         <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -11,9 +34,11 @@ export function IncomesCard() {
                 <ArrowUp color="hsl(var(--muted-foreground))" />
             </CardHeader>
             <CardContent>
-                <div className="text-2xl font-bold">R$2,231.89</div>
+                <div className="text-2xl font-bold">
+                    {incomes}
+                </div>
                 <p className="text-xs text-muted-foreground">
-                    +20.1%  desde o último mês
+                    {percentageChange}  desde o último mês
                 </p>
             </CardContent>
         </Card>
